0abb7634a92a75a1ce7dba3eb078de60f7f46100
jewel1269 <jewelmia2330@gamil.com>
2026-09-12 01:14:58 +0600

    fix deploy issues

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   headers: { "Content-Type": "application/json" },
 });
 
